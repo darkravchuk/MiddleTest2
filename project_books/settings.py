0a006c5d9a92c10be4bb2e7be5677e6a609564b6
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-05-31 13:25:24 +0300

    Add .env file

--- project_books/settings.py
+++ project_books/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zhrx)90pu7&8k*kbk)0*nl)tx%%_03d_k!)h7*ltjp0(s_5juv'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
