cdb7424269df15b95364d83816d9cc2aa02619da
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-05-31 12:46:01 +0300

    Add books_view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Author
+from django.db.models import Count
+
+
+def books_view(request):
+    # Отримуємо 5 останніх доданих до бази даних книг
+    latest_books = Book.objects.order_by('-publication_date')[:5]
+
+    # Передаємо список останніх книг у шаблон та відображаємо його
+    return render(request, 'books.html', {'books': latest_books})
+
 
